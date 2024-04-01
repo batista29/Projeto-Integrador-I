24c9651a741c670bfcd9096824e91ffbf2e6f03f
esterAmaral <163193914+esterAmaral@users.noreply.github.com>
2024-04-01 20:49:24 -0300

    Update cad_prod.py

--- desktop/cad_prod.py
+++ desktop/cad_prod.py
@@ -1,24 +1,30 @@
 # cadastro de produtos
 
-print("**************************************************")
-print("Cadastro do produto \n")
+print("\t"*5 + "-"*100)
+print("\t"*10 +" CADASTRO DE PRODUTOS \n")
 # código do produto
-codprod = input("Digite o código do produto: ")
+codprod = input("\t"*7 +"Digite o código do produto: ")
 # nome do produto
-nomep = input("Nome do produto: ")
+nomep = input("\t"*7 +"Nome do produto: ")
 # descrição do produto
-descp = input("Digite uma breve descrição do produto:")
+descp = input("\t"*7 +"Digite uma breve descrição do produto:")
 # custo produto
-custop = float(input("Custo do produto: "))
+custop = float(input("\t"*7 +"Custo do produto: "))
 # custo fixo do produto
-custofixop = float(input("Custo fixo do produto (%): "))
+custofixop = float(input("\t"*7 +"Custo fixo do produto (%): "))
 
 # comissão vendas
-cv = float(input("Comissão de venda (%): "))
+cv = float(input("\t"*7 +"Comissão de venda (%): "))
 
-imposto = float(input("Imposto sobre a venda (%): "))
+imposto = float(input("\t"*7 +"Imposto sobre a venda (%): "))
 # rentabilidade
-rentab = float(input("Rentabilidade (%): "))
+rentab = float(input("\t"*7 +"Rentabilidade (%): "))
+
+pv = custop/(1 -((custofixop + cv + imposto + rentab)/100))
+pv = round(pv,2)
+print("\t"*11 + "-"*9)
+print("\t"*11 + f"|R$ {pv}|")
+print("\t"*11 + "-"*9)
 
 pv = custop/(1 -((custofixop + cv + imposto + rentab)/100))
 print(pv)
