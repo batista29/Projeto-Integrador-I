e212257a28cca401c780e2bacec8460dca74535b
mariana-cgsilva <cgsilva.mariana@gmail.com>
2024-04-02 10:59:03 -0300

    Att: Tabela de banco de dados

--- bcd/TabelaEstoque.sql
+++ bcd/TabelaEstoque.sql
@@ -1,10 +1,26 @@
 CREATE TABLE Estoque(
     id_prod INTEGER PRIMARY KEY NOT NULL,
-    nome_prod VARCHAR2 NOT NULL,
-    desc_prod VARCHAR2 NOT NULL,
+    nome_prod VARCHAR2(255) NOT NULL,
+    desc_prod VARCHAR2(255) NOT NULL,
     custo_prod NUMBER(10,2) NOT NULL,
     custo_fixo NUMBER(10,2) NOT NULL,
     comissao NUMBER (5,3) NOT NULL,
     imposto NUMBER (10,2) NOT NULL,
     rentabilidade NUMBER (6,3) NOT NULL
 );
+
+describe Estoque;
+
+select * from Estoque;
+
+INSERT into Estoque values (36,'geladeira','geladeira grande branca',5000,200,10,300,20);
+
+UPDATE Estoque SET custo_fixo = 250 WHERE id_prod = 36;
+
+DELETE FROM Estoque WHERE id_prod = 36;
+
+INSERT into Estoque values (36,'geladeira','geladeira grande branca',36,15,12,5,20);
+
+INSERT into Estoque values (37,'geladeira','geladeira pequena',1,1,1,1,0);
+
+
