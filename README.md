b63f7b475737d014e4a78242ff027d764e4f94ae
NATA BATISTA FERNANDES <nata.bf@puccampinas.edu.br>
2024-04-09 11:23:33 -0300

    Iniciando ligação com o banco

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Projeto-Integrador-I
 Primeiro projeto integrador - PUC-Campinas
+
+## Instalações
+
+* Antes de executar instale pelo CMD: pip install cx_Oracle
+
+* Site explicando: https://www.profissionaloracle.com.br/2023/08/02/acesso-ao-banco-de-dados-oracle-com-python-guia-detalhado-e-exemplos-praticos/
---
+++ desktop/testeBCD.py
@@ -0,0 +1,30 @@
+import cx_Oracle
+
+# Informações de conexão
+username = 'BD150224429'
+password = 'Cuehg3'
+host = 'BD-ACD'  # Endereço do servidor
+port = 1521        # Número da porta padrão do Oracle
+service_name = 'XE'  # Ou substitua por SID, caso esteja usando
+
+# String de conexão
+dsn = cx_Oracle.makedsn(host, port, service_name=service_name)
+
+# Conectar ao banco de dados
+connection = cx_Oracle.connect(username, password, dsn)
+
+# Criar um cursor
+cursor = connection.cursor()
+
+# Consulta SQL
+sql = 'select nome_prod from Estoque'
+
+# Executar a consulta
+cursor.execute(sql)
+
+# Recuperar os resultados
+for nome_prod in cursor:
+    print(f'{nome_prod}')
+
+# Fechar o cursor
+cursor.close()
