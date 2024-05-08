2e46d75067249f52b21880af1705e5536efc8de2
Geovana Barbosa <geovana.sb@puccampinas.edu.br>
2024-05-08 10:34:31 -0300

    Atualização codigo no excluir, não funciona o delete

--- desktop/main.py
+++ desktop/main.py
@@ -8,7 +8,7 @@ connection = oracledb.connect(
 
 cursor = connection.cursor()
 
-option = int(input("Digite 1 (Cadastrar produto) ou 2 (Listar produto): "))
+option = int(input("Digite 1 (Cadastrar produto), 2 (Listar produto) ou 4 (Excluir produto): "))
 
 #FUNÇÃO QUE FAZ OS CALCULOS
 def calcular_valores(custo_prod, custo_fixo, comissao, imposto, rentabilidade):
@@ -132,8 +132,69 @@ elif option == 2:
         else:
             print("\t"*7+"Lucro:\tPrejuízo")
         print("\n")
+
     cursor.close()
     connection.close()
+
+
+elif option == 4:
+
+    print("Excluir")
+    
+    id=int(input("Digite o id do produto que deseja excluir: "))
+
+    connection.commit()
+    cursor.execute("SELECT * FROM Estoque where id_prod = :id", {'id': id})
+
+    for row in cursor: 
+        #Para imprimir dados tal como foram inseridos no SQL:
+
+        id_prod = row[0] 
+        nome_prod = row[1]
+        desc_prod = row[2]
+        custo_prod = row[3] 
+        custo_fixo = row[4]
+        comissao = row[5]
+        imposto = row[6] 
+        rentabilidade = row[7]
+
+        pv, receitaBruta, porcentcusto_prod, porcentReceitaBruta, porcentOutroCusto, valor_custoFixo, valor_comissao, valor_imposto, valor_outroCusto, valor_rentabilidade, porcentRentab = calcular_valores(
+            custo_prod, custo_fixo, comissao, imposto, rentabilidade)
+         
+        
+        print("\t"*7 + f"{id_prod}:\t {nome_prod}\t {desc_prod}")
+        print("\t"*7 +"Descrição"+"\t"*4+"Valor"+"\t"*3+"%")
+        print("\t"*7 +f"A. Preço de venda"+"\t"*3+f"R$ {pv:.2f}"+"\t"*3+"100%")
+        print("\t"*7 +f"B. Custo de Aquisição (Fornecedor)\tR$ {custo_prod:.2f}"+"\t"*2+f"{round(porcentcusto_prod):.2f}%")
+        print("\t"*7 +f"C. Receita Bruta (A-B)"+"\t"*3+f"R$ {receitaBruta:.2f}"+"\t"*2+f"{round(porcentReceitaBruta):.2f}%")
+        print("\t"*7 +f"D.Custo Fixo/Administrativo"+"\t"*2+f"R$ {valor_custoFixo:.2f}"+"\t"*2+f"{round(valor_custoFixo/pv*100):.2f}%")
+        print("\t"*7 +f"E.Comissão de Vendas"+"\t"*3+f"R$ {valor_comissao:.2f}"+"\t"*3+f"{round(valor_comissao/pv*100):.2f}%")
+        print("\t"*7 +f"F.Impostos"+"\t"*4+f"R$ {valor_imposto:.2f}"+"\t"*3+f"{round(valor_imposto/pv*100):.2f}%")
+        print("\t"*7 +f"G. Outros Custos (D+E+F)"+"\t"*2+f"R$ {valor_outroCusto:.2f}"+"\t"*3+f"{round(valor_outroCusto/pv*100):.2f}%")
+        print("\t"*7 +f"H. Rentabilidade"+"\t"*3+f"R$ {valor_rentabilidade:.2f}"+"\t"*3+f"{round(valor_rentabilidade/pv*100):.2f}%\n")
+
+        escolha=input("Tem certeza que deseja excluir o id {id}? (S/N)")
+        escolha2=escolha.upper()
+        
+        if escolha2=='S':
+            try:
+                cursor.execute("DELETE FROM Estoque WHERE id_prod= :id", {'id': id})
+                print("Item excluido com sucesso")
+                connection.commit() 
+            except oracledb.Error as erro:
+                print(erro)
+        else:
+            print("Seu item não foi excluido")
+
+    connection.commit()
+    cursor.close()   
+    connection.close()
+
+
+
+
+
+
 
 
 
